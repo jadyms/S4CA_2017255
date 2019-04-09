01d8b9d0903e2bf230c92d0c5a3057675feac97e
jadyms <33762825+jadyms@users.noreply.github.com>
2019-04-09 11:34:55 +0100

    V3

---
+++ src/CustomerView.java
@@ -0,0 +1,66 @@
+
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author JadyMartins
+ */
+public class CustomerView extends JFrame{
+    
+    HomeView homeView;
+        
+    public CustomerView(){
+        
+        homeView = new HomeView();
+        
+    }
+    public void AddNewCustomerPanel(){
+      
+         JPanel myPanel2 = new JPanel();
+         String b1 = "Add Customer";
+         String b2 = "Manage Existing Customer";
+        // String b3 = "Manage Rental"; 
+         //String b4 = "Loyalty Card";
+        //Panel 2 for buttons
+      
+        //Buttons for Panel 2
+        JButton btn1 = new JButton(b1);
+        JButton btn2 = new JButton(b2);
+      //  JButton btn3 = new JButton(b3);
+       // JButton btn4 = new JButton(b4);
+        
+        //Set Action command
+        btn1.setActionCommand(b1);
+        btn2.setActionCommand(b2);
+       // btn3.setActionCommand(b3);
+        //btn4.setActionCommand(b4);
+        
+       //Add Action Listener as Anonymous Class
+       btn1.addActionListener(new ActionListener(){
+           public void actionPerformed(ActionEvent e) {
+       
+               System.out.println("Add Customer");
+           }});
+
+                //Add Buttons to Panel 2
+        myPanel2.add(btn1);
+        myPanel2.add(btn2);
+        //myPanel2.add(btn3);
+        //myPanel2.add(btn4);
+        
+           //Panel 2 layout
+        GridLayout mgr = new GridLayout(1,2);
+        myPanel2.setLayout(mgr);
+        
+        homeView.mainPanel("Select an option", myPanel2, "Logout");
+
+             
+        
+    }
+    
+}
--- src/HomeView.java
+++ src/HomeView.java
@@ -1,6 +1,8 @@
 
 import java.awt.BorderLayout;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JFrame;
@@ -12,10 +14,11 @@ import static javax.swing.WindowConstants.DISPOSE_ON_CLOSE;
 public class HomeView extends JFrame {
     
       JDialog frame;
+    
       
     public HomeView(){
-      mainPanel("Select an option", "Manage Customers", "Manage Titles", "Loyalty Card", "Manage Rental", "Logout");
-        
+      
+        HomePanel();
     }
     private void newFrame(int h, int w) {
        
@@ -28,17 +31,15 @@ public class HomeView extends JFrame {
         frame.repaint();
     }
     
-    
-     public void mainPanel(String message, String b1, String b2, String b3, String b4, String b5) {
-        newFrame(600, 600);
-        //Panel 1 for welcome message
-        JPanel myPanel1 = new JPanel();
-        //message for Panel 1
-        JLabel myLabel = new JLabel(message);//according to method calling it
-        
-        
+    public void HomePanel(){
+      
+         JPanel myPanel2 = new JPanel();
+         String b1 = "Manage Customer";
+         String b2 = "Manage Titles";
+         String b3 = "Manage Rental"; 
+         String b4 = "Loyalty Card";
         //Panel 2 for buttons
-        JPanel myPanel2 = new JPanel();
+      
         //Buttons for Panel 2
         JButton btn1 = new JButton(b1);
         JButton btn2 = new JButton(b2);
@@ -48,18 +49,43 @@ public class HomeView extends JFrame {
         //Add Action command
         btn1.setActionCommand(b1);
         btn2.setActionCommand(b2);
-        btn2.setActionCommand(b3);
-        btn2.setActionCommand(b4);
-             
+        btn3.setActionCommand(b3);
+        btn4.setActionCommand(b4);
+
+        btn1.addActionListener(new ActionListener(){
+           public void actionPerformed(ActionEvent e) {
+       
+               System.out.println("Manage Customer");
+           }});
+                //Add Buttons to Panel 2
+        myPanel2.add(btn1);
+        myPanel2.add(btn2);
+        myPanel2.add(btn3);
+        myPanel2.add(btn4);
+        
+           //Panel 2 layout
+        GridLayout mgr = new GridLayout(2,2);
+        myPanel2.setLayout(mgr);
+        
+        mainPanel("Select an option", myPanel2, "Logout");
+        
+    }
+    
+     public void mainPanel(String message, JPanel myPanel2, String b5) {
+        newFrame(600, 600);
+        //Panel 1 for welcome message
+        JPanel myPanel1 = new JPanel();
+        //message for Panel 1
+        JLabel myLabel = new JLabel(message);//according to method calling it
+        
+        
+        
         //Panel 3 for footer buttons
         JPanel myPanel3 = new JPanel();
         
         //Logout button
          JButton btn5 = new JButton(b5);
         
-        //Add Action command
-        btn1.setActionCommand(b5);       
-      
         //Instance of Customer Controller that holds Action Events     
        Logout l = new Logout();
         btn5.addActionListener(l);
@@ -68,19 +94,13 @@ public class HomeView extends JFrame {
         //Add Welcome message to Panel 1
         myPanel1.add(myLabel);
         
-        //Add Buttons to Panel 2
-        myPanel2.add(btn1);
-        myPanel2.add(btn2);
-        myPanel2.add(btn3);
-        myPanel2.add(btn4);
+
         
           //Add Buttons to Panel 2
           
         myPanel3.add(btn5);
 
-        //Panel 2 layout
-        GridLayout mgr = new GridLayout(2,2);
-        myPanel2.setLayout(mgr);
+     
         //Frame Layout
         BorderLayout manager = new BorderLayout();
         frame.setLayout(manager);
